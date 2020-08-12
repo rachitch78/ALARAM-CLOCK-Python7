5117d282782510103bd7d954667275bacce10f85
Rachit Choudhary <49128494+rachitch78@users.noreply.github.com>
2020-08-12 19:22:56 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,5 +1,10 @@
 # alarm_clock.py
-
+# Description: A simple Python program to make the computer act 
+# like an alarm clock. Start it running from the command line 
+# with a command line argument specifying the duration in minutes 
+# after which to sound the alarm. It will sleep for that long, 
+# and then beep a few times. Use a duration of 0 to test the 
+# alarm immediiately, e.g. for checking that the volume is okay.
 
 
 import sys
@@ -45,3 +50,7 @@ try:
 except KeyboardInterrupt:
     print "Interrupted by user"
     sys.exit(1)
+    
+    
+    
+    
